a90be57edc330ea3e2e217930a9edebcbabc1b4a
Daniel Nieland <danielnieland7@gmail.com>
2021-11-24 00:40:02 -0800

    first html/css project (guided)

---
+++ javascript-learning/script.js
@@ -0,0 +1,73 @@
+console.log("Basic JavaScript practice");
+//Comments = exactly the same as C++ /* even multiline */
+//JavaScript is DYNAMICALLY typed
+
+// Creating variable using var keyword
+// var varName =  'test';
+
+// // Creating variable using let keyword
+// let varName =  'test';
+
+// Creating variable using let keyword
+// const varName =  'test';
+
+// Strings
+// const me = "Daniel";
+// const backticks = `Hello, ${me}`;
+
+// console.log(typeof backticks);
+
+// Numbers -- untyped
+// const first = 5;
+// const string = 'Hello';
+// const second = 10;
+
+// const result = first / second;
+
+// console.log(result);
+
+// Objects
+// const person = {
+//     name: 'Daniel',
+//     age: 21,
+// }
+
+// console.log(person)
+
+// Array
+// const arr = [1, 2, 3, 4];
+
+// console.log(arr);
+
+// const date = new Date();
+
+// console.log(date);
+
+// Comparisons
+// const a = 10;
+// const b = 4;
+// Strict equality/inequality
+// console.log(a === b);
+// console.log(a !== b);
+
+// In general ALWAYS use 3 equals, unless good reason for 2
+
+// Functions
+// function square(number) {
+//   return number * number;
+// }
+
+// const result = square(5);
+
+// console.log(result);
+
+// Arrow functions
+// Do not create their own "this"
+
+// If only one return statement, you can omit the return statement
+// If only one parameter, you can omit the parentheses
+// const square = (number) => number * number;
+
+// const result = square(20);
+
+// console.log(result);
